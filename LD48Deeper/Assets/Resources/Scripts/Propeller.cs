2dc8af0e36dcf2ffd447a09633d15c1bf0956c8f
python-dragon <thomas.e.gruenewald@gmail.com>
2021-04-24 14:16:19 -0500

    propler mosly works

---
+++ LD48Deeper/Assets/Resources/Scripts/Propeller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propeller : MonoBehaviour
+{
+    public float thrust;
+    public float efficency;
+    public float energy; //this is a placeholder
+    public GameObject submarine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W) && energy > 0.0)
+        {
+            Vector2 dir = transform.GetComponent<Rigidbody2D>().GetRelativeVector(Vector2.up);
+            transform.GetComponent<Rigidbody2D>().AddForce(dir * thrust);
+            energy = energy - efficency;
+            print("vrrrooom");
+        }
+       
+        
+    }
+}
