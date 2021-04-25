95e32ac9183d18b54199dfe707ba9253ce007d46
tgruenewald <terrygruenewald@gmail.com>
2021-04-24 22:32:16 -0600

    can restore cursor and delete cells

--- LD48Deeper/Assets/DesignCell.cs
+++ LD48Deeper/Assets/DesignCell.cs
@@ -7,6 +7,10 @@ public class DesignCell : MonoBehaviour
     public GameObject block = null;
     public float smooth = 1f;
     private Quaternion targetRotation;
+
+    DesignGrid designGrid = null;
+
+    string this_current_block = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,32 +19,43 @@ public class DesignCell : MonoBehaviour
 
     private void OnMouseDown()
     {
-        DesignGrid designGrid = transform.parent.gameObject.transform.parent.gameObject.GetComponent<DesignGrid>();
+        designGrid = transform.parent.gameObject.transform.parent.gameObject.GetComponent<DesignGrid>();
+        designGrid.current_cell = this.gameObject;
         Debug.Log("Putting down a block:  " + designGrid.current_block);
-        if (block != null) 
-        {
+        if (designGrid.current_block == null || (this_current_block != null && this_current_block == designGrid.current_block)) {
             Destroy(block);
+            this_current_block = null;
         }
-        block = (GameObject) Instantiate(Resources.Load("prefab/"+ designGrid.current_block+"_part"), GetComponent<Transform>().position, GetComponent<Transform>().rotation) ;
-        targetRotation = block.transform.rotation;
+        else {
+            if (block != null) 
+            {
+                Destroy(block);
+            }
+            block = (GameObject) Instantiate(Resources.Load("prefab/"+ designGrid.current_block+"_part"), GetComponent<Transform>().position, GetComponent<Transform>().rotation) ;
+            this_current_block = designGrid.current_block;
+            targetRotation = block.transform.rotation;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("Pressing A");
-            targetRotation *=  Quaternion.AngleAxis(5, Vector3.forward);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Debug.Log("Pressing D");
-            targetRotation *=  Quaternion.AngleAxis(-5, Vector3.forward);
-        }
-        if (block != null)
-        {
-            block.transform.rotation= Quaternion.Lerp (block.transform.rotation, targetRotation , 10 * smooth * Time.deltaTime);
+        if (designGrid != null && designGrid.current_cell == this.gameObject) {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("Pressing A");
+                targetRotation *=  Quaternion.AngleAxis(5, Vector3.forward);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Debug.Log("Pressing D");
+                targetRotation *=  Quaternion.AngleAxis(-5, Vector3.forward);
+            }
+            if (block != null)
+            {
+                block.transform.rotation= Quaternion.Lerp (block.transform.rotation, targetRotation , 10 * smooth * Time.deltaTime);
+            }
         }
 
     }
--- LD48Deeper/Assets/DesignGrid.cs
+++ LD48Deeper/Assets/DesignGrid.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DesignGrid : MonoBehaviour
 {
     public string current_block = null;
+    public GameObject current_cell = null;
     // Start is called before the first frame update
     void Start()
     {
