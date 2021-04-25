db1d3d9c7e3d2af196fb9999866946d3e00ef23c
tgruenewald <terrygruenewald@gmail.com>
2021-04-24 21:38:58 -0600

    parts rotate

--- LD48Deeper/Assets/DesignCell.cs
+++ LD48Deeper/Assets/DesignCell.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DesignCell : MonoBehaviour
 {
     public GameObject block = null;
+    public float smooth = 1f;
+    private Quaternion targetRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,26 @@ public class DesignCell : MonoBehaviour
             Destroy(block);
         }
         block = (GameObject) Instantiate(Resources.Load("prefab/"+ designGrid.current_block+"_part"), GetComponent<Transform>().position, GetComponent<Transform>().rotation) ;
+        targetRotation = block.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Pressing A");
+            targetRotation *=  Quaternion.AngleAxis(5, Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Pressing D");
+            targetRotation *=  Quaternion.AngleAxis(-5, Vector3.forward);
+        }
+        if (block != null)
+        {
+            block.transform.rotation= Quaternion.Lerp (block.transform.rotation, targetRotation , 10 * smooth * Time.deltaTime);
+        }
+
     }
 }
